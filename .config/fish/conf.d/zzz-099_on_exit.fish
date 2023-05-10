787b7998639e4b5b1ee0ee98beacce110cb25d16
Vasyl Zuziak <zvasylvv@gmail.com>
2023-05-10 10:45:21 -0700

    autoupdate of configs in 'dotfiles'

--- .config/fish/conf.d/zzz-099_on_exit.fish
+++ .config/fish/conf.d/zzz-099_on_exit.fish
@@ -1,20 +1,8 @@
 # vim: set filetype=sh : ~
 
-# delete-or-exit
-# https://stackoverflow.com/questions/39728790/bash-equivalent-bash-logout-for-fish-shell
-# https://fishshell.com/docs/current/cmds/bind.html?highlight=delete+exit
-# ...
-
 function detach_logout_tasks
     silent_detach logout_tasks
 end
-
-# function on_exit --on-process-exit $fish_pid
-#     if status is-interactive
-# 	#detach_logout_tasks >/dev/null 2>&1
-# 	touch /tmp/on_exit_fish_($fish_pid)
-#     end
-# end
 
 function on_exit --on-process %self
     detach_logout_tasks
