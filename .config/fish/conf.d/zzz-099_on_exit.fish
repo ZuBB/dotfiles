62d6828a9ab1369f43d7f5490f6e716abc4085e0
Vasyl Zuziak <zvasylvv@gmail.com>
2023-05-10 10:26:28 -0700

    autoupdate of configs in 'dotfiles'

--- .config/fish/conf.d/zzz-099_on_exit.fish
+++ .config/fish/conf.d/zzz-099_on_exit.fish
@@ -9,9 +9,13 @@ function detach_logout_tasks
     silent_detach logout_tasks
 end
 
-function on_exit --on-event fish_exit
-    if status is-interactive
-        detach_logout_tasks >/dev/null 2>&1
-    end
-end
+# function on_exit --on-process-exit $fish_pid
+#     if status is-interactive
+# 	#detach_logout_tasks >/dev/null 2>&1
+# 	touch /tmp/on_exit_fish_($fish_pid)
+#     end
+# end
 
+function on_exit --on-process %self
+    detach_logout_tasks
+end
