949bce2d60b0bf8e75d7ff036fb353160e684cc8
Vasyl Zuziak <zvasylvv@gmail.com>
2023-05-29 14:49:35 -0700

    autoupdate of configs in 'dotfiles'

--- .config/fish/conf.d/zzz-012_on_login.fish
+++ .config/fish/conf.d/zzz-012_on_login.fish
@@ -1,8 +1,7 @@
 # vim: set filetype=sh : ~
 
 function detach_login_tasks
-    #nohup vcsh pull -q &>/dev/null &
-    command nohup vcsh pull -q </dev/null &>/dev/null & disown
+    nohup2 vcsh pull -q
 end
 
 if status is-interactive
