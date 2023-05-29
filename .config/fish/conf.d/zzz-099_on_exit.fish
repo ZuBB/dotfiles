106764ef78ebc55fb1d4135d6b2aaf27b7a10788
Vasyl Zuziak <zvasylvv@gmail.com>
2023-05-29 14:56:12 -0700

    autoupdate of configs in 'dotfiles'

--- .config/fish/conf.d/zzz-099_on_exit.fish
+++ .config/fish/conf.d/zzz-099_on_exit.fish
@@ -5,19 +5,15 @@ function detach_logout_tasks
         vcsh "$repo" commit -qam "autoupdate of configs in '$repo'"
     end
 
-    #nohup2 vcsh push -q
-    #nohup vcsh push -q &
-    command nohup vcsh push -q </dev/null >/dev/null 2>&1 & disown
+    nohup vcsh push -q &>/dev/null &
 end
 
-function nohup2
-    #command nohup $argv </dev/null >/dev/null 2>&1 & disown
-    command nohup $argv </dev/null >/dev/null 2>&1 & disown
-end
+# https://github.com/fish-shell/fish-shell/issues/4010#issuecomment-439721767
+# function nohup2
+#    command nohup $argv </dev/null >/dev/null 2>&1 & disown
+# end
 
 function on_exit --on-process %self
-    #nohup2 detach_logout_tasks >/dev/null 2>&1
     detach_logout_tasks
-    #nohup2 logout_tasks
 end
 
