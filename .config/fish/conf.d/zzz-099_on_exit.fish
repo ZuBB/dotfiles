0546e0559d0d45283c85a000061e06c42158a52c
Vasyl Zuziak <zvasylvv@gmail.com>
2023-08-11 22:31:31 -0700

    autoupdate of configs in 'dotfiles'

--- .config/fish/conf.d/zzz-099_on_exit.fish
+++ .config/fish/conf.d/zzz-099_on_exit.fish
@@ -10,7 +10,7 @@ function detach_logout_tasks
     # https://github.com/fish-shell/fish-shell/issues/4010#issuecomment-439721767
     # https://stackoverflow.com/questions/23898623/nohup-cant-detach-from-console
     # reattach-to-user-namespace nohup vcsh push -q </dev/null &>/dev/null & disown
-    reattach-to-user-namespace nohup vcsh push -q </dev/null &>/dev/null & disown %1
+    reattach-to-user-namespace nohup vcsh push -q </dev/null &>/dev/null & disown
 end
 
 function mytest
