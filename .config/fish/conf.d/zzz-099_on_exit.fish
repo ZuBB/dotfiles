671f1b2d660200a212541849d4f69d9c461fbe1e
Vasyl Zuziak <zvasylvv@gmail.com>
2023-08-11 23:19:45 -0700

    autoupdate of configs in 'dotfiles'

--- .config/fish/conf.d/zzz-099_on_exit.fish
+++ .config/fish/conf.d/zzz-099_on_exit.fish
@@ -2,8 +2,16 @@
 # https://matrix.to/#/!YLTeaulxSDauOOxBoR:matrix.org/$1sc5KBTXkQoMgF4naB5BGeUVwtyaAu431jE5NPQmsIg?via=matrix.org&via=gitter.im&via=zwanenburg.info
 
 function detach_logout_tasks
+    #git diff-index --quiet HEAD
+
+    #test $status -eq 0; and return 0
+
+    #if test $status -eq 0
+        #return 0
+    #end
+
     for repo in (vcsh list)
-        vcsh "$repo" commit -qam "autoupdate of configs in '$repo'"
+        vcsh "$repo" commit -qam "autoupdate of configs in '$repo'" &>/dev/null
     end
 
     # https://github.com/fish-shell/fish-shell/issues/4010#issuecomment-439721767
