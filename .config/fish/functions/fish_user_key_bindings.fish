891895ca245baf83e875b7a3d1b7681508b5b189
Vasyl Zuziak <zvasylvv@gmail.com>
2023-08-12 00:48:13 -0700

    autoupdate of configs in 'dotfiles'

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,6 @@
 function fish_user_key_bindings
   bind \cw backward-kill-bigword
+  # https://github.com/fish-shell/fish-shell/issues/2904
+  bind \cc 'echo; commandline | cat; commandline ""; commandline -f repaint'
 end
 
