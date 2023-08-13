c189b73265b6ed64b7daee95486e998db0df2cf8
Vasyl Zuziak <zvasylvv@gmail.com>
2023-08-13 16:03:01 -0700

    autoupdate of configs in 'dotfiles'

--- .local/bin/vcsh-logout.sh
+++ .local/bin/vcsh-logout.sh
@@ -3,8 +3,7 @@
 for repo in `vcsh list`; do
     vcsh run "${repo}" git diff-index --quiet HEAD
 
-    if [ $? -ne 0 ]
-    then
+    if [ $? -ne 0 ]; then
         vcsh run "${repo}" git commit -qam "autoupdate of configs in '${repo}'";
     fi
 done
