c73c4ef6136e21432803a386dc0a508748c2b55b
Vasyl Zuziak <zvasylvv@gmail.com>
2023-08-13 17:45:55 -0700

    autoupdate of configs in 'dotfiles'

---
+++ .config/fish/conf.d/zzz-042_programming-languages.fish
@@ -0,0 +1,10 @@
+# JavaScript
+
+# https://github.com/Schniz/fnm#fish-shell
+command -q fnm && fnm env --use-on-cd | source
+
+
+# Python
+
+# https://github.com/pyenv/pyenv#set-up-your-shell-environment-for-pyenv
+command -q pyenv && pyenv init - | source
