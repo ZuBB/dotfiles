0cba9b929eb11b0d673b56e3b6b00eacae9bc840
Vasyl Zuziak <zvasylvv@gmail.com>
2023-08-13 17:05:42 -0700

    autoupdate of configs in 'dotfiles'

--- .config/fish/conf.d/zzz-099_on_exit.fish
+++ .config/fish/conf.d/zzz-099_on_exit.fish
@@ -7,6 +7,7 @@ function detach_logout_tasks
     # https://github.com/kovidgoyal/kitty/discussions/6544
     # https://unix.stackexchange.com/questions/266565/daemonize-a-process-in-shell
     # reattach-to-user-namespace nohup vcsh push -q </dev/null &>/dev/null & disown
+    # TODO check how this behaves on Linux
     daemonize $HOME/.local/bin/vcsh-logout.sh
 end
 
