82708c8806260213e7bab77e43f0b0612d819209
Vasyl Zuziak <zvasylvv@gmail.com>
2023-08-18 13:39:37 -0700

    autoupdate of configs in 'dotfiles'

---
+++ .config/fish/conf.d/zzz-010_brew_pathes_in_fish.fish
@@ -0,0 +1,13 @@
+function add_brew_pathes_to_path
+    set -gx HOMEBREW_PREFIX "/opt/homebrew";
+    set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+    set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
+    set -q PATH; or set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
+    set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
+    set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
+end
+
+if status is-login
+    add_brew_pathes_to_path
+end
+
