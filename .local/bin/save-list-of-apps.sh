d4243d8b4311db9a82f8fb2b077baa7a67123605
Vasyl Zuziak <zvasylvv@gmail.com>
2023-08-18 12:43:35 -0700

    autoupdate of configs in 'dotfiles'

--- .local/bin/save-list-of-apps.sh
+++ .local/bin/save-list-of-apps.sh
@@ -4,11 +4,7 @@ DEST_PATH=$HOME/.local/share/installed-apps-list
 
 mkdir -p $DEST_PATH
 
-brew leaves -r > $DEST_PATH/brew-apps
-
-brew list --cask -1 > $DEST_PATH/casks-apps
-
-mas list > $DEST_PATH/appstore-apps
+brew bundle dump -qf --file $DEST_PATH/Brewfile
 
 ls -1 /Applications/ > $DEST_PATH/system-apps
 
