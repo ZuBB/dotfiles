b347b2b576b789f97ebb5b64001cdb38155c6765
Vasyl Zuziak <zvasylvv@gmail.com>
2023-08-27 20:11:43 -0700

    autoupdate of configs in 'dotfiles'

--- .local/bin/save-list-of-apps.sh
+++ .local/bin/save-list-of-apps.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-DEST_PATH=$HOME/.local/share/installed-apps-list/macos
+SYSTEM=$(uname -o)
+SYSTEM_NAME="$(hostname)-$SYSTEM"
+DEST_PATH=$HOME/.local/share/system-state/$SYSTEM_NAME/installed-apps
 
 mkdir -p $DEST_PATH
 
-/opt/homebrew/bin/brew bundle dump -qf --file $DEST_PATH/Brewfile
+if [ -e /opt/homebrew/bin/brew ]; then
+    /opt/homebrew/bin/brew bundle dump -qf --file $DEST_PATH/Brewfile
+fi
 
-ls -1 /Applications/ > $DEST_PATH/system-apps
-
-ls -1 $HOME/Applications/ > $DEST_PATH/user-apps
+if [ "$SYSTEM" == "Darwin" ]; then
+    ls -1 /Applications/ > $DEST_PATH/system-apps
+    ls -1 $HOME/Applications/ > $DEST_PATH/user-apps
+fi
 
