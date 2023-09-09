bc871bfe21421e2f1a8c8b9e09b06510ff4d560e
Vasyl Zuziak <zvasylvv@gmail.com>
2023-09-08 21:25:00 -0700

    autoupdate of configs in 'dotfiles'

--- .config/fish/conf.d/zzz-011_pathes.fish
+++ .config/fish/conf.d/zzz-011_pathes.fish
@@ -1,3 +1,6 @@
+# XDG cheat-sheet
+# https://gist.github.com/roalcantara/107ba66dfa3b9d023ac9329e639bc58c
+
 function add_pathes_to_path
     # own executables
     fish_add_path -aP ~/.local/bin
@@ -11,4 +14,5 @@ end
 if status is-login
     add_pathes_to_path
 end
+
 
