faee71f4b677601c99a756689c8286c3b1f0e934
Vasyl Zuziak <zvasylvv@gmail.com>
2023-09-10 09:37:12 -0700

    autoupdate of configs in 'dotfiles'

--- .local/bin/save-list-of-apps.sh
+++ .local/bin/save-list-of-apps.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
-SYSTEM_TYPE=$(uname -o | tr '[:upper:]' '[:lower:]')
+OS_TYPE=$(uname -o | tr '[:upper:]' '[:lower:]')
 HOST_NAME=$(hostname -s)
-DEST_PATH=$HOME/.local/share/system-state/$SYSTEM_TYPE/$HOST_NAME
+DEST_PATH=$HOME/.local/share/system-state/$OS_TYPE/$HOST_NAME
+BREW_EXE_PATH=/opt/homebrew/bin/brew
 
 mkdir -p $DEST_PATH
 
-if [ -e /opt/homebrew/bin/brew ]; then
-    HOMEBREW_NO_AUTO_UPDATE=1 /opt/homebrew/bin/brew bundle dump -qf --brews --cask --tap --mas --file $DEST_PATH/Brewfile
-fi
-
-if [ "$SYSTEM_TYPE" == "Darwin" ]; then
+if [ "$OS_TYPE" == "darwin" ]; then
     ls -1 /Applications/ > $DEST_PATH/installed-system-apps
     ls -1 $HOME/Applications/ > $DEST_PATH/installed-user-apps
+fi
+
+if [ -e $BREW_EXE_PATH ]; then
+    HOMEBREW_NO_AUTO_UPDATE=1 $BREW_EXE_PATH bundle dump -qf --brews --cask --tap --mas --file $DEST_PATH/Brewfile
 fi
 
