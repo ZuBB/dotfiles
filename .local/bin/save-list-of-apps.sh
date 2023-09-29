85816518e9967ca5d92d9e870babb56b9675a6b5
Vasyl Zuziak <zvasylvv@gmail.com>
2023-09-28 18:08:16 -0700

    autoupdate of configs in 'dotfiles'

--- .local/bin/save-list-of-apps.sh
+++ .local/bin/save-list-of-apps.sh
@@ -2,17 +2,27 @@
 
 OS_TYPE=$(uname -o | tr '[:upper:]' '[:lower:]')
 HOST_NAME=$(hostname -s)
-DEST_PATH=$HOME/.local/share/system-state/$OS_TYPE/$HOST_NAME
+
 BREW_EXE_PATH=/opt/homebrew/bin/brew
 
-mkdir -p $DEST_PATH
+COMMON_DEST_PATH=$HOME/.local/share/system-state/$OS_TYPE/$HOST_NAME
+CUR_DEST_PATH=$COMMON_DEST_PATH/current
+AT_DEST_PATH=$COMMON_DEST_PATH/all-time
+
+mkdir -p $COMMON_DEST_PATH/{current,all-time}
+
+
+# CURRENT DATA
 
 if [ "$OS_TYPE" == "darwin" ]; then
-    ls -1 /Applications/ > $DEST_PATH/installed-system-apps
-    ls -1 $HOME/Applications/ > $DEST_PATH/installed-user-apps
+    ls -1 /Applications/ > $CUR_DEST_PATH/installed-system-apps;
+    ls -1 $HOME/Applications/ > $CUR_DEST_PATH/installed-user-apps;
 fi
 
 if [ -e $BREW_EXE_PATH ]; then
-    HOMEBREW_NO_AUTO_UPDATE=1 $BREW_EXE_PATH bundle dump -qf --brews --cask --tap --mas --file $DEST_PATH/Brewfile
+    HOMEBREW_NO_AUTO_UPDATE=1 $BREW_EXE_PATH bundle dump -qf --brews --cask --tap --mas --file $CUR_DEST_PATH/Brewfile;
 fi
 
+# ALL-TIME DATA
+
+#{ ls -1 /Applications/; cat $AT_DEST_PATH/installed-system-apps } | sort -u > $AT_DEST_PATH/installed-system-apps;
