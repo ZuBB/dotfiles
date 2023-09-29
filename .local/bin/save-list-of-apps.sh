4e7e2bc6f14d88cc1152066d422afbaa1810508d
Vasyl Zuziak <zvasylvv@gmail.com>
2023-09-28 18:48:53 -0700

    autoupdate of configs in 'dotfiles'

--- .local/bin/save-list-of-apps.sh
+++ .local/bin/save-list-of-apps.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
+ARCH=$(uname -m)
 OS_TYPE=$(uname -o | tr '[:upper:]' '[:lower:]')
 HOST_NAME=$(hostname -s)
 
-BREW_EXE_PATH=/opt/homebrew/bin/brew
-
-COMMON_DEST_PATH=$HOME/.local/share/system-state/$OS_TYPE/$HOST_NAME
+BASE_DEST_PATH=$HOME/.local/share/system-state/
+COMMON_DEST_PATH=$HOME/.local/share/system-state/$ARCH/$OS_TYPE/$HOST_NAME
 CUR_DEST_PATH=$COMMON_DEST_PATH/current
 AT_DEST_PATH=$COMMON_DEST_PATH/all-time
 
@@ -19,8 +19,9 @@ if [ "$OS_TYPE" == "darwin" ]; then
     ls -1 $HOME/Applications/ > $CUR_DEST_PATH/installed-user-apps;
 fi
 
-if [ -e $BREW_EXE_PATH ]; then
-    HOMEBREW_NO_AUTO_UPDATE=1 $BREW_EXE_PATH bundle dump -qf --brews --cask --tap --mas --file $CUR_DEST_PATH/Brewfile;
+
+if [ -e $HOME/.local/bin/save-list-of-brew-apps.sh ]; then
+    $HOME/.local/bin/save-list-of-brew-apps.sh $CUR_DEST_PATH;
 fi
 
 # ALL-TIME DATA
