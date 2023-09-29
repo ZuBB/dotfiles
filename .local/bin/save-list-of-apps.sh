61aabadbc09f55425a0865ab28081a1b04444f51
Vasyl Zuziak <zvasylvv@gmail.com>
2023-09-28 18:59:41 -0700

    autoupdate of configs in 'dotfiles'

--- .local/bin/save-list-of-apps.sh
+++ .local/bin/save-list-of-apps.sh
@@ -5,7 +5,7 @@ OS_TYPE=$(uname -o | tr '[:upper:]' '[:lower:]')
 HOST_NAME=$(hostname -s)
 
 BASE_DEST_PATH=$HOME/.local/share/system-state/
-COMMON_DEST_PATH=$HOME/.local/share/system-state/$ARCH/$OS_TYPE/$HOST_NAME
+COMMON_DEST_PATH=$BASE_DEST_PATH/$ARCH/$OS_TYPE/$HOST_NAME
 CUR_DEST_PATH=$COMMON_DEST_PATH/current
 AT_DEST_PATH=$COMMON_DEST_PATH/all-time
 
@@ -14,15 +14,14 @@ mkdir -p $COMMON_DEST_PATH/{current,all-time}
 
 # CURRENT DATA
 
-if [ "$OS_TYPE" == "darwin" ]; then
-    ls -1 /Applications/ > $CUR_DEST_PATH/installed-system-apps;
-    ls -1 $HOME/Applications/ > $CUR_DEST_PATH/installed-user-apps;
+if [ -e $HOME/.local/bin/save-list-of-mac-apps.sh ]; then
+    $HOME/.local/bin/save-list-of-mac-apps.sh $CUR_DEST_PATH;
 fi
-
 
 if [ -e $HOME/.local/bin/save-list-of-brew-apps.sh ]; then
     $HOME/.local/bin/save-list-of-brew-apps.sh $CUR_DEST_PATH;
 fi
+
 
 # ALL-TIME DATA
 
