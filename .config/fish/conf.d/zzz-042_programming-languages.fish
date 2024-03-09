e9dcc6124465cce879068a70af4730f27ef636d2
Vasyl Zuziak <zvasylvv@gmail.com>
2024-03-08 20:12:31 -0800

    autoupdate of configs in 'dotfiles'

--- .config/fish/conf.d/zzz-042_programming-languages.fish
+++ .config/fish/conf.d/zzz-042_programming-languages.fish
@@ -8,3 +8,7 @@ command -q fnm && fnm env --use-on-cd | source
 
 # https://github.com/pyenv/pyenv#set-up-your-shell-environment-for-pyenv
 command -q pyenv && pyenv init - | source
+
+# https://vfox.lhan.me/guides/quick-start.html#_2-hook-vfox-to-your-shell
+# vfox activate fish | source
+
