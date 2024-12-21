bb3326d16f9d43a23bddebeb722b8e1f819ba75f
Vasyl Zuziak <zvasylvv@gmail.com>
2024-12-21 11:13:09 +0100

    autoupdate of content in 'dotfiles' repo

--- .config/fish/conf.d/zzz-042_programming-languages.fish
+++ .config/fish/conf.d/zzz-042_programming-languages.fish
@@ -12,10 +12,10 @@ command -q pyenv && pyenv init - | source
 
 # Golang
 if command -q go
-    set -gx GOBIN=$HOME/.local/bin
-    set -gx GOPATH=$HOME/.local/share/go
-    set -gx GOCACHE=$HOME/.cache/go-build
-    set -gx GOMODCACHE=$HOME/.cache/go-mod
+    set -gx GOBIN $HOME/.local/bin
+    set -gx GOPATH $HOME/.local/share/go
+    set -gx GOCACHE $HOME/.cache/go-build
+    set -gx GOMODCACHE $HOME/.cache/go-mod
     set -gx GOROOT (go env GOROOT)
     set -gx PATH $GOBIN $PATH
 end
