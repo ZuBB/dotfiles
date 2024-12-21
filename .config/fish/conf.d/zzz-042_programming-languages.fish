1a02c302d8c2b625908f1397629cddee97d20f17
Vasyl Zuziak <zvasylvv@gmail.com>
2024-12-21 10:53:33 +0100

    autoupdate of content in 'dotfiles' repo

--- .config/fish/conf.d/zzz-042_programming-languages.fish
+++ .config/fish/conf.d/zzz-042_programming-languages.fish
@@ -9,6 +9,18 @@
 # https://github.com/pyenv/pyenv#set-up-your-shell-environment-for-pyenv
 command -q pyenv && pyenv init - | source
 
+
+# Golang
+if command -q go
+    set -g GOBIN (go env GOBIN)
+    if test -n "$GOBIN_LOCAL"
+        set -x PATH $GOBIN $PATH
+    end
+end
+
+
+# Any lang
+
 # https://vfox.lhan.me/guides/quick-start.html#_2-hook-vfox-to-your-shell
 command -q vfox && vfox activate fish | source
 
