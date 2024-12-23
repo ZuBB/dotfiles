cf58186336909347e5a0522d72728078f0c20f74
Vasyl Zuziak <zvasylvv@gmail.com>
2024-12-23 18:45:40 +0100

    autoupdate of content in 'dotfiles' repo

--- .config/fish/conf.d/zzz-012_on_login.fish
+++ .config/fish/conf.d/zzz-012_on_login.fish
@@ -3,7 +3,19 @@
 function detach_login_tasks
     nohup vcsh pull -q </dev/null &>/dev/null & disown
 
-    mkdir -p /tmp/screens
+    create_screens_dir
+end
+
+function create_screens_dir
+    set -l os (uname)
+
+    if test "$os" = Darwin
+        mkdir -p /tmp/screens
+#   else if test "$os" = Linux
+#       add_linux_brew_pathes_to_path
+    else
+        echo "OS $os is not supported"
+    end
 end
 
 if status is-interactive
