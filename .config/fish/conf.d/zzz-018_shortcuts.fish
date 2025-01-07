412461e4b018af1a62e2f18242c015d64248692f
Vasyl Zuziak <zvasylvv@gmail.com>
2025-01-07 17:08:27 +0100

    autoupdate of content in 'dotfiles' repo

--- .config/fish/conf.d/zzz-018_shortcuts.fish
+++ .config/fish/conf.d/zzz-018_shortcuts.fish
@@ -1,19 +1,10 @@
 # vim: set filetype=sh : ~
 
-#set version (fish --version | string split ' ' | string take 1)
-
-#if test (string sub -s 1 -l 1 $version) -ge 4
-   #bind alt-backspace backward-kill-word
-#end
-
-#   set fish_version (fish --version | string split ' ' | string join '' | string replace -r '.*version ' '')
-
-#   if test (string sub -s 1 -l 1 $fish_version) -ge 4
-#      bind alt-backspace backward-kill-word
-#   end
-
+# https://github.com/fish-shell/fish-shell/issues/10926
+# https://github.com/fish-shell/fish-shell/discussions/11018
 set fish_version (fish --version | string replace -r 'fish, version ' '')
 
 if test (string sub -s 1 -l 1 $fish_version) -ge 4
    bind alt-backspace backward-kill-word
 end
+
