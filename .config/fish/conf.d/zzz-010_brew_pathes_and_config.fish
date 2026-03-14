59602e9741753bd97ae2a3834cf190f3ec1edfea
Vasyl Zuziak <zvasylvv@gmail.com>
2026-03-14 12:01:29 +0100

    autoupdate of content in 'dotfiles' repo

--- .config/fish/conf.d/zzz-010_brew_pathes_and_config.fish
+++ .config/fish/conf.d/zzz-010_brew_pathes_and_config.fish
@@ -1,9 +1,11 @@
 function add_macos_brew_pathes_to_path
     # nothing fancy. just output of official command `brew shellenv`
-    set -gx HOMEBREW_PREFIX "/opt/homebrew";
-    set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
-    set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
-    fish_add_path -gP "/opt/homebrew/bin" "/opt/homebrew/sbin";
+    set --global --export HOMEBREW_PREFIX "/opt/homebrew";
+    set --global --export HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+    set --global --export HOMEBREW_REPOSITORY "/opt/homebrew";
+
+    fish_add_path --global --move --path "/opt/homebrew/bin" "/opt/homebrew/sbin";
+
     ! set -q MANPATH; and set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
     ! set -q INFOPATH; and set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
 end
